366d65babcb3328baff113b28287434eb65963ef
Joel Brodusch <joel.brodusch@gmail.com>
2026-06-24 15:03:38 +0200

    Little fixes

--- main.py
+++ main.py
@@ -22,10 +22,11 @@ except Exception as e:
 def get_token() -> str:
     """
     Exécute une demande de token grâce au client_id et au client_secret fournit par l'environnement.
+
     :return: Le token pour faire la requête à l'API.
     """
     try:
-        auth_string = client_id + ":" + client_secret
+        auth_string = f"{client_id}:{client_secret}"
         auth_bytes = auth_string.encode('utf-8')
         auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
 
@@ -38,7 +39,7 @@ def get_token() -> str:
         data = {"grant_type": "client_credentials"}
         result = post(url, headers=headers, data=data)
         json_result = json.loads(result.content)
-        token = json_result["access_token"]
+        token = str(json_result["access_token"])
         return token
     except Exception as e:
         print(e)
@@ -58,9 +59,11 @@ def get_playlist_tracks(token: str, id_playlist: str, offset: int = 0) -> Any:
     """
     Effectue la requête via l'API de Spotify pour obtenir la liste des musiques dans la playlist passée en paramètre.
     La requête extrait au maximum 100 musiques. Attention à bien calibrer l'offset en conséquence.
+
     :param token: Token qui permet de réaliser la requête
     :param id_playlist: ID de la playlist
     :param offset: Index à partir duquel les musiques sont scrapées
+
     :return: La liste des musiques scrapées sour format JSON
     """
     url = "https://api.spotify.com/v1/playlists/" + id_playlist + \
@@ -72,7 +75,6 @@ def get_playlist_tracks(token: str, id_playlist: str, offset: int = 0) -> Any:
     # Si on souhaite avoir le résultat dans un fichier JSON :
     # with open("playlist.json", "w", encoding="utf-8") as f:
     #    json.dump(json_result, f, indent=4)
-
     return json_result
 
 
@@ -80,6 +82,7 @@ def json_to_txt(items: Any) -> None:
     """
     Écrit dans le fichier playlist.txt les musiques dont leurs dates d'ajout est postérieure à la dernière musique
     ajoutée au fichier.
+
     :param items: La liste JSON des musiques acquises par la requête.
     """
     script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -97,15 +100,16 @@ def json_to_txt(items: Any) -> None:
                     else:
                         s += artist["name"] + " "
                 s += "— " + item["track"]["name"] + "\n"
-            s += "\nDate du dernier ajout :\n" + items["items"][n-1]["added_at"]
+            s += "\nDate du dernier ajout :\n"\
+                f"{items["items"][n - 1]["added_at"]}"
             f.write(s)
             print("Fin de la première écriture.")
     else:
         with open(filename, "r") as f:
             lines = f.readlines()
-            last_line = lines[len(lines)-1]
+            last_line = lines[len(lines) - 1]
 
-        if (last_line < items["items"][n-1]["added_at"]):
+        if (last_line < items["items"][n - 1]["added_at"]):
             print("Début d'écriture.")
             with open(filename, "w") as f:
                 s = ""
@@ -115,7 +119,7 @@ def json_to_txt(items: Any) -> None:
                         for artist in item["track"]["artists"]:
                             n_artists = len(item["track"]["artists"])
                             if n_artists > 1:
-                                if i == n_artists-1 :
+                                if i == n_artists - 1:
                                     s += artist["name"] + " "
                                 else:
                                     s += artist["name"] + ", "
@@ -123,7 +127,8 @@ def json_to_txt(items: Any) -> None:
                             else:
                                 s += artist["name"] + " "
                         s += "— " + item["track"]["name"] + "\n\n"
-                s += "\nDate du dernier ajout :\n" + items["items"][n - 1]["added_at"]
+                s += "\nDate du dernier ajout :\n"\
+                    f"{items["items"][n - 1]["added_at"]}"
                 f.write(s)
                 print("Fin d'écriture.")
                 print(str(len(lines) - 2) + " musiques ont été ajoutées.")
